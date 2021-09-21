b09f985fcaf8e5a2de414c7c03074b85e1dcf9d1
Julian Wilson <58565920+juju669@users.noreply.github.com>
2021-09-21 13:06:18 -0400

    added bank-route to api

--- controllers/index.js
+++ controllers/index.js
@@ -4,6 +4,7 @@ const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
 const lawyerRoutes = require('./lawyer-routes.js');
+const bankRoutes = require('./bank-routes');
 
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
