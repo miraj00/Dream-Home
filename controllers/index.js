42489d636e2247248825fc893ba75df327690b87
SC2021 <walterlaw19@gmail.com>
2021-09-22 22:43:21 -0400

    router.create message

--- controllers/index.js
+++ controllers/index.js
@@ -4,9 +4,12 @@ const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
 const dashboardRoutes = require('./dashboard-routes.js');
 const lawyerRoutes = require('./lawyer-routes.js');
+const reachOutRoutes = require('./reachOut-routes');
 
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
 router.use('/lawyer', lawyerRoutes)
+router.use('/', reachOutRoutes);
+
 module.exports = router;
--- controllers/reachOut-routes.js
+++ controllers/reachOut-routes.js
@@ -1,9 +1,26 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
+const { ReachOut, User } = require('../models/');
 
 router.post('/', (req, res) => {
-    
-})
+    ReachOut.create({
+        your_name: req.body.your_name,
+        Your_contact_number: req.body.Your_contact_number,
+        your_name: req.body.your_name
+    })
+    .then(userMessage => res.json(userMessage))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+// router.get('/', (req, res) => {
+//     console.log('it works');
+//     User.findAll({
+
+//     })
+// })
 
 
 module.exports = router;
