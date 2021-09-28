2e3ba8b3db17d2584900be02b87c5ad4d478e0a4
Mona Lisa <soaresmaria181@yahoo.com>
2021-09-28 19:21:06 -0400

    final page added

--- public/survey/index.js
+++ public/survey/index.js
@@ -43,14 +43,68 @@ Survey
                             text: "Service is easy to use"
                         }
                     ]
+                   },{
+                    type: "rating",
+                    name: "satisfaction",
+                    title: "How satisfied are you with the Service?",
+                    isRequired: true,
+                    mininumRateDescription: "Not Satisfied",
+                    maximumRateDescription: "Completely satisfied"
+                   },{
+                    type: "rating",
+                    name: "recommend friends",
+                    visibleIf: "{satisfaction} > 3",
+                    title: "How likely are you to recommend the Service to a friend or co-worker?",
+                    mininumRateDescription: "Will not recommend",
+                    maximumRateDescription: "I will recommend"
+                   },{
+                    type: "comment",
+                    name: "suggestions",
+                    title: "What would make you more satisfied with the Service?"
                    }
                ]
+        }, {
+            questions: [
+                {
+                    type: "radiogroup",
+                    name: "price to competitors",
+                    title: "Compared to our competitors, do you feel the Service is",
+                    choices: ["Priced about the same", "More expensive", "Not sure"]
+                }, {
+                    type: "radiogroup",
+                    name: "price",
+                    title: "Do you feel our current price is merited by the current market?",
+                    choices: ["correct|Yes, the price is about right", "high|No, the price is too high"]
+                }, {
+                    type: "multipletext",
+                    name: "pricelimit",
+                    title: "What is the... ",
+                    items: [
+                        {
+                            name: "mostamount",
+                            title: "Most amount you would every pay"
+                        }, {
+                            name: "leastamount",
+                            title: "The least amount you would feel comfortable paying"
+                        }
+                    ]
+                }
+            ]
+        }, {
+            questions: [
+                {
+                    type: "text",
+                    name: "email",
+                    title: "Thank you for taking our survey. Your survey is almost complete, please enter your email address in the box below if you wish to participate in our drawing, then press the 'Submit' button."
+                }
+            ]
         }
-        ]
-    };
+    ]
+};
+        
+    
 
 
     window.survey = new Survey.Model(json);
-
     
 $("#surveyElement").Survey({model: survey});
